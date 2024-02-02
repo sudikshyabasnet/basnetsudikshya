fbcbde5690768aecb3ee24cfa9a75434ae2450df
sudikshyabasnet <104345294+sudikshyabasnet@users.noreply.github.com>
2024-02-02 20:42:51 +0545

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,87 @@
+$(document).ready(function(){
+    $(".header-textcolumn").waypoint(function () {
+        $(".header-textcolumn").addClass(
+            "animate__animated animate__fadeInUp"
+        );
+    }
+    ,{offset:"80%"});
+});
+
+$(document).ready(function(){
+    $(".intro-image").waypoint(function () {
+        $(".intro-image").addClass(
+            "animate__animated animate__fadeInUp"
+        );
+    }
+    ,{offset:"80%"});
+});
+$(document).ready(function(){
+    $(".about-col-1").waypoint(function () {
+        $(".about-col-1").addClass(
+            "animate__animated animate__fadeInBottomLeft"
+        );
+    }
+    ,{offset:"80%"});
+});
+
+$(document).ready(function(){
+    $(".about-col-2").waypoint(function () {
+        $(".about-col-2").addClass(
+            "animate__animated animate__fadeInBottomRight"
+        );
+    }
+    ,{offset:"80%"});
+});
+
+$(document).ready(function(){
+    $(".services-list div").waypoint(function () {
+        $(".services-list div").addClass(
+            "animate__animated animate__fadeInUp"
+        );
+    }
+    ,{offset:"80%"});
+});
+
+$(document).ready(function(){
+    $(".services-list div").waypoint(function () {
+        $(".services-list div").addClass(
+            "animate__animated animate__fadeInUp"
+        );
+    }
+    ,{offset:"80%"});
+});
+$(document).ready(function(){
+    $(".work").waypoint(function () {
+        $(".work").addClass(
+            "animate__animated animate__fadeInRight"
+        );
+    }
+    ,{offset:"80%"});
+});
+
+$(document).ready(function(){
+    $(".btn").waypoint(function () {
+        $(".btn").addClass(
+            "animate__animated animate__fadeInUp"
+        );
+    }
+    ,{offset:"80%"});
+});
+
+$(document).ready(function(){
+    $(".contact-left").waypoint(function () {
+        $(".contact-left").addClass(
+            "animate__animated animate__fadeInLeft"
+        );
+    }
+    ,{offset:"80%"});
+});
+$(document).ready(function(){
+    $(".contact-right").waypoint(function () {
+        $(".contact-right").addClass(
+            "animate__animated animate__fadeInRight"
+        );
+    }
+    ,{offset:"80%",
+});
+});
